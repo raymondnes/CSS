4990681a513b9e40df97521b08b1aa2b00a09de4
Raymond Toritseju <raynelreign@gmail.com>
2023-02-26 01:00:02 +0400

    added css styles

--- flip-book/script.js
+++ flip-book/script.js
@@ -25,7 +25,9 @@ let maxLocation = numOfPages + 1;
 
 function openBook()
 {
-
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-180px)";
+    nextBtn.style.transform = "translateX(180px)";
 }
 
 function closeBook()
@@ -42,30 +44,39 @@ function goNextPage()
             case 1:
                 openBook();
                 paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
                 break;
             case 2:
                 paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
                 break;
             case 3:
                 paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
                 break;
             case 4:
                 paper4.classList.add("flipped");
+                paper4.style.zIndex = 4;
                 break;
             case 5:
                 paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
                 break;
             case 6:
                 paper6.classList.add("flipped");
+                paper6.style.zIndex = 6;
                 break;
             case 7:
                 paper7.classList.add("flipped");
+                paper7.style.zIndex = 7;
                 break;
             case 8:
                 paper8.classList.add("flipped");
+                paper8.style.zIndex = 8;
                 break;
             case 9:
                 paper9.classList.add("flipped");
+                paper9.style.zIndex = 9;
                 closeBook();
                 break;
             default:
